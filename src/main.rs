24f9a97fd15adfce6d82e7871022ff7fff012910
Ian <pbl9815@gmail.com>
2022-03-08 15:27:26 -0600

    Wrote a VGA buffer writer.

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,13 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 // This function is the new entry point, since we don't have an underlying runtime to call main
 // and the linker looks for a function named `_start` by default.
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // The VGA buffer is located at the address 0xb8000
-    // Cast the interger 0xb8000 into a raw pointer
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
